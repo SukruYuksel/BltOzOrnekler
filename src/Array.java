426f2778f5913deae5273008fd78eb492d70f844
SukruYuksel <yksl3743dc@gmail.com>
2024-01-07 00:59:23 +0100

    Reverse Array eklendi

---
+++ src/Array.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Array {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Verilen Arrayin Reversesisini bulma");
+        int[] array1 = {1,2,3,4,5,6,7,8,9,0};
+       List<Integer> list2=new ArrayList<>();
+        for (int i = 0; i < array1.length; i++) {
+            list2.add(array1[i]);
+
+        }
+        System.out.println("Arrays.toString(array1) = " + Arrays.toString(array1));
+        System.out.println("Array ="+list2);
+       Collections.reverse(list2);
+        System.out.println("Reverse array ="+list2);
+
+    }
+}
