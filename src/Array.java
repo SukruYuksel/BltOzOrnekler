5cb40a9a046746c91fb4b11cca0b819e78fff7ac
Sükrü Yüksel <65497504+SukruYuksel@users.noreply.github.com>
2024-01-07 01:35:50 +0100

    Yeni aciklama eklendi

--- src/Array.java
+++ src/Array.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Array {
 
     public static void main(String[] args) {
+        ///Yeni Satirlar eklendi
+          System.out.println("Arrayi Array Liste Cevirerek  Islemi daha mantikli hale getirip yazabilirisin");
         System.out.println("Verilen Arrayin Reversesisini bulma");
         int[] array1 = {1,2,3,4,5,6,7,8,9,0};
        List<Integer> list2=new ArrayList<>();
